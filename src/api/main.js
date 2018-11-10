a09a873283fa5c1b5f54fed90e1ac969b514fc93
ivanbodnar <ibodnar@outlook.com>
2018-11-10 17:32:44 -0300

    Agregado routes -> themes.routes

---
+++ src/api/main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/themes.routes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/', routes);
+
+app.listen(3000, () => console.log('App listening on port 3000'));
