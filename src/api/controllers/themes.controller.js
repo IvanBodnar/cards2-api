c2520f86ea36dbdbc4fcb3db8fa410e4668d1d54
ivanbodnar <ibodnar@outlook.com>
2018-11-13 10:11:13 -0300

    Agregados try catch en controllers

--- src/api/controllers/themes.controller.js
+++ src/api/controllers/themes.controller.js
@@ -1,19 +1,26 @@
 const Theme = require('../models/theme.model');
 
-// TODO try catch
 
 const saveTheme = async (req, res, next) => {
-  const name = req.body.name;
-  const theme = new Theme({name: name});
-  theme.save()
-      .then(
-          theme => res.send(theme)
-      )
+    try {
+        const name = req.body.name;
+        const theme = new Theme({name: name});
+        const savedTheme = await theme.save();
+        res.send(savedTheme);
+    }
+    catch (e) {
+        res.status(404).send(e);
+    }
 };
 
 const themes = async (req, res, next) => {
-  const themes = await Theme.find();
-  res.send(themes);
+    try {
+        const themes = await Theme.find();
+        res.send(themes);
+    }
+    catch (e) {
+        res.status(404).send(e);
+    }
 };
 
 module.exports = {
