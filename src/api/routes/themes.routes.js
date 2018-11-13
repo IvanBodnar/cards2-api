043ae440389d2302fc129deae14555c43a4361bd
ivanbodnar <ibodnar@outlook.com>
2018-11-13 01:05:02 -0300

    Agregadas routes get themes y post theme

--- src/api/routes/themes.routes.js
+++ src/api/routes/themes.routes.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const controllers = require('../controllers/themes.controller');
 
 const router = express.Router();
+
+router.get('/themes', controllers.themes);
+router.post('/theme', controllers.saveTheme);
 
 module.exports = router;
