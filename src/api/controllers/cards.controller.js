489fcfe4d44942d2b9b9fa6ad8297eec66c40364
ivan <ibodnar@outlook.com>
2018-11-15 11:19:28 -0300

    Bugfix: el contructor de Card model toma un objeto

--- src/api/controllers/cards.controller.js
+++ src/api/controllers/cards.controller.js
@@ -14,16 +14,16 @@ const saveCard = async (req, res, next) => {
     try {
         let front, back, themeId;
         [front, back, themeId] = [req.body.front, req.body.back, req.body.themeId];
-        const card = new Card(
+        const card = new Card({
             front,
             back,
             themeId
-        );
+        });
         const savedCard = await card.save();
         res.send(savedCard);
     }
     catch (e) {
-
+        res.send(e);
     }
 };
 
